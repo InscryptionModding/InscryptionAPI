11b64a197284f8fef5e936d1d50e3a42a24fc855
Scott Wilson <scottwilson0903@gmail.com>
2021-11-08 22:52:41 +0000

    Fixed default card MetaData

--- Utils/CardUtils.cs
+++ Utils/CardUtils.cs
@@ -7,19 +7,19 @@ namespace CardLoaderPlugin.lib
 {
     public class CardUtils
     {
-        
+
         public static readonly Vector2 DefaultVector2 = new Vector2(0.5f, 0.5f);
         public static readonly Rect DefaultCardArtRect = new Rect(0.0f, 0.0f, 114.0f, 94.0f);
         public static readonly Rect DefaultCardPixelArtRect = new Rect(0.0f, 0.0f, 41.0f, 28.0f);
-        
+
         public static List<CardAppearanceBehaviour.Appearance> getRareAppearance =
             new List<CardAppearanceBehaviour.Appearance>() { CardAppearanceBehaviour.Appearance.RareCardBackground };
 
         public static List<CardMetaCategory> getNormalCardMetadata = new List<CardMetaCategory>()
-            { CardMetaCategory.ChoiceNode };
-        
+            { CardMetaCategory.ChoiceNode, CardMetaCategory.TraderOffer };
+
         public static List<CardMetaCategory> getRareCardMetadata = new List<CardMetaCategory>()
-            { CardMetaCategory.ChoiceNode, CardMetaCategory.Rare };
+            { CardMetaCategory.Rare };
 
         public static Texture2D loadImage(string pathCardArt)
         {
