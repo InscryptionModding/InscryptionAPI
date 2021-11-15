084d041f2b3c1b119a212cbd0a6b166f93a19383
julian-perge <julianperge@gmail.com>
2021-11-15 10:00:58 -0500

    Added AbilityInfoUtils.cs

---
+++ Utils/AbilityInfoUtils.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using DiskCardGame;
+using UnityEngine;
+
+namespace APIPlugin
+{
+	public class AbilityInfoUtils
+	{
+		public static AbilityInfo CreateInfoWithDefaultSettings(string rulebookName, string rulebookDescription)
+		{
+			AbilityInfo info = ScriptableObject.CreateInstance<AbilityInfo>();
+			info.powerLevel = 0;
+			info.rulebookName = rulebookName;
+			info.rulebookDescription = rulebookDescription;
+			info.metaCategories = new List<AbilityMetaCategory>()
+			{
+				AbilityMetaCategory.Part1Modular, AbilityMetaCategory.Part1Rulebook
+			};
+
+			return info;
+		}
+	}
+}
