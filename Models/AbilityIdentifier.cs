845f797eb0b4f65b59d45b1a9848ce7dcc810be5
unknown <Martin.A.D.Harvey@hotmail.co.uk>
2021-11-16 23:10:42 +0000

    Fix repo mismatch

--- Models/AbilityIdentifier.cs
+++ Models/AbilityIdentifier.cs
@@ -7,7 +7,7 @@ namespace APIPlugin
 {
 	public class AbilityIdentifier
 	{
-    public static List<AbilityIdentifier> ids = new List<AbilityIdentifier>();
+    private static List<AbilityIdentifier> ids = new List<AbilityIdentifier>();
 		private string guid;
     private string name;
     public Ability id;
