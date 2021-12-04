810af3841e26396a9f071b295422b2141dbb994c
IngoHHacks <ingohhacks@gmail.com>
2021-12-04 13:04:57 +0100

    Region randomization for the first tier

--- Patches/RegionProgression.cs
+++ Patches/RegionProgression.cs
@@ -10,25 +10,27 @@ namespace API.Patches
 	{
 		public static void Prefix(ref RegionProgression ___instance)
 		{
-			if(___instance == null)
+			if (___instance == null)
 			{
 				RegionProgression official = ResourceBank.Get<RegionProgression>("Data/Map/RegionProgression");
 
 				// If no custom region is defined for the default region, but custom encounters for that region exist, create one
 				for (int t = 0; t <= 3; t++)
-                {
+				{
 					if (NewEncounter.encounters.Exists(encounter => (encounter.regionName == official.regions[t][0].name)))
-                    {
+					{
 						if (!CustomRegion.regions.Exists(region => (region.tier == t && region.name == official.regions[t][0].name)))
 						{
 							new CustomRegion(official.regions[t][0].name);
-                        }
-                    }
-                }
+						}
+					}
+				}
 
-				foreach(CustomRegion region in CustomRegion.regions){
+				foreach (CustomRegion region in CustomRegion.regions)
+				{
 					bool found = false;
-					foreach (List<RegionData> tier in official.regions) {
+					foreach (List<RegionData> tier in official.regions)
+					{
 						RegionData officialRegion = tier.Find((RegionData x) => x.name == region.name);
 						if (officialRegion != null)
 						{
@@ -41,10 +43,10 @@ namespace API.Patches
 									officialRegion.encounters.Add(encounter.encounterBlueprintData);
 								}
 								if (encounter.bossPrep)
-                                {
+								{
 									Plugin.Log.LogInfo($"Loaded custom encounter {encounter.name} as boss prep encounter into region {region.name}");
 									officialRegion.bossPrepEncounter = encounter.encounterBlueprintData;
-                                }
+								}
 							}
 							if (customEncounters.Count > 0)
 							{
@@ -56,18 +58,19 @@ namespace API.Patches
 						}
 					}
 					if (!found)
-                    {
+					{
 						Plugin.Log.LogInfo($"Could not find region {region.name} to modify");
 					}
 				}
 
-				foreach(NewRegion region in NewRegion.regions){
+				foreach (NewRegion region in NewRegion.regions)
+				{
 					while (region.tier >= official.regions.Count)
 					{
 						official.regions.Add(new List<RegionData>());
 					}
 					official.regions[region.tier].Add(region.region);
-					
+
 					List<NewEncounter> customEncounters = NewEncounter.encounters.FindAll(x => x.regionName == region.region.name);
 					foreach (NewEncounter encounter in customEncounters)
 					{
