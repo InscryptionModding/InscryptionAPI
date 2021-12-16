cdc1532a84cfdf6338f6c190131b7168b0862f63
Aaron Robinson <aaronrobin1234@gmail.com>
2021-12-15 20:42:36 -0600

    Create new sln and project

---
+++ InscryptionAPI/InscryptionAPIPlugin.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx;
+using HarmonyLib;
+
+namespace InscryptionAPI;
+
+[BepInPlugin(ModGUID, ModName, ModVer)]
+public class InscryptionAPIPlugin : BaseUnityPlugin
+{
+    public const string ModGUID = "cyanist.inscryption.api";
+    public const string ModName = "InscryptionAPI";
+    public const string ModVer = "2.0.0";
+
+    public void Awake()
+    {
+        Harmony.CreateAndPatchAll(typeof(InscryptionAPIPlugin).Assembly, ModGUID);
+    }
+}
