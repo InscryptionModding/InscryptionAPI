9f1f3a5e42603355139e98a7c2ae2af15b868a88
divisionbyz0rro <divisionbyzorro@gmail.com>
2022-02-09 19:40:19 -0600

    Added a "get values" iterator for extendable enums

--- InscryptionAPI/Guid/GuidManager.cs
+++ InscryptionAPI/Guid/GuidManager.cs
@@ -25,6 +25,26 @@ public static class GuidManager
         return reverseMapper[number];
     }
 
+    unsafe public static List<T> GetValues<T>() where T : unmanaged, System.Enum
+    {
+        List<T> itemList = new();
+        foreach(T item in Enum.GetValues(typeof(T)))
+            itemList.Add(item);
+
+        string startKey = typeof(T).Name + "_";
+        foreach (var item in ModdedSaveManager.SaveData.SaveData[InscryptionAPIPlugin.ModGUID])
+        {
+            if (item.Key.StartsWith(startKey))
+            {
+                int enumVal = int.Parse(item.Value);
+                T convertedEnumVal = *(T*)&enumVal;
+                itemList.Add(convertedEnumVal);
+            }
+        }
+        
+        return itemList;
+    }
+
     unsafe public static T GetEnumValue<T>(string guid, string value) where T : unmanaged, System.Enum
     {
         if (sizeof(T) != sizeof(int))
