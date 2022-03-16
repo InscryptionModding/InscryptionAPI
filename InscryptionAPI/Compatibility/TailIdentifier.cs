73194e39d22ddcbc12272fd6ecd5d75fd5468888
julian-perge <julianperge@gmail.com>
2022-03-15 22:59:28 -0400

    Reverted v1 changes

--- InscryptionAPI/Compatibility/TailIdentifier.cs
+++ InscryptionAPI/Compatibility/TailIdentifier.cs
@@ -1,60 +1,62 @@
 using DiskCardGame;
+using InscryptionAPI.Card;
 using InscryptionAPI.Helpers;
 using UnityEngine;
 
-namespace APIPlugin;
-
-[Obsolete("Use CardManager and CardInfo extension methods", true)]
-public class TailIdentifier
+namespace APIPlugin
 {
-    internal readonly string name;
-    internal readonly CardModificationInfo mods;
-    internal readonly Texture2D tailLostTex;
-    private TailParams _tail;
+    [Obsolete("Use CardManager and CardInfo extension methods", true)]
+	public class TailIdentifier
+	{
+		internal string name;
+		internal CardModificationInfo mods;
+		internal Texture2D tailLostTex;
+		private TailParams tail;
 
-    public TailParams Tail
-    {
-        get
-        {
-            if (this._tail == null)
-                SetParams(CardLoader.GetCardByName(this.name));
+		public TailParams Tail
+		{
+			get
+			{
+				if (this.tail == null)
+                    SetParams(CardLoader.GetCardByName(this.name));
+				
+				return this.tail;
+			}
+		}
 
-            return this._tail;
-        }
-    }
+		public TailIdentifier(string name, Texture2D tailLostTex = null, CardModificationInfo mods = null)
+		{
+			this.name = name;
+			this.mods = mods;
+			this.tailLostTex = tailLostTex;
+		}
 
-    public TailIdentifier(string name, Texture2D tailLostTex = null, CardModificationInfo mods = null)
-    {
-        this.name = name;
-        this.mods = mods;
-        this.tailLostTex = tailLostTex;
-    }
+		private void SetParams(CardInfo card)
+		{
+			TailParams tail = new TailParams();
 
-    private void SetParams(CardInfo card)
-    {
-        TailParams iTail = new TailParams();
+			if (this.tailLostTex is not null)
+			{
+				this.tailLostTex.name = this.name;
+				this.tailLostTex.filterMode = FilterMode.Point;
 
-        if (this.tailLostTex is not null)
-        {
-            this.tailLostTex.name = this.name;
-            this.tailLostTex.filterMode = FilterMode.Point;
+				tail.tailLostPortrait = TextureHelper.ConvertTexture(this.tailLostTex, TextureHelper.SpriteType.CardPortrait);
+				tail.tailLostPortrait.name = this.name;
+			}
 
-            iTail.tailLostPortrait = this.tailLostTex.ConvertTexture(TextureHelper.SpriteType.CardPortrait);
-            iTail.tailLostPortrait.name = this.name;
-        }
+			tail.tail = card;
 
-        iTail.tail = card;
+			if (this.mods != null)
+			{
+				tail.tail.mods.Add(this.mods);
+			}
 
-        if (this.mods != null)
-        {
-            iTail.tail.mods.Add(this.mods);
-        }
+			this.tail = tail;
+		}
 
-        this._tail = iTail;
-    }
-
-    public override string ToString()
-    {
-        return name;
-    }
+		public override string ToString()
+		{
+			return name;
+		}
+	}
 }
