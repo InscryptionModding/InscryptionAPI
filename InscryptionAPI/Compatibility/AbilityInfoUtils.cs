4ecde86521df6c733ae36090633c8f6012becb11
julian-perge <julianperge@gmail.com>
2022-03-15 20:15:45 -0400

    Fixing imports and set eberything to file-scoped namespaces

--- InscryptionAPI/Compatibility/AbilityInfoUtils.cs
+++ InscryptionAPI/Compatibility/AbilityInfoUtils.cs
@@ -1,23 +1,22 @@
 using DiskCardGame;
 using UnityEngine;
 
-namespace APIPlugin
-{
-    [Obsolete("Use AbilityManager and extension methods instead", true)]
-	public class AbilityInfoUtils
-	{
-		public static AbilityInfo CreateInfoWithDefaultSettings(string rulebookName, string rulebookDescription)
-		{
-			AbilityInfo info = ScriptableObject.CreateInstance<AbilityInfo>();
-			info.powerLevel = 0;
-			info.rulebookName = rulebookName;
-			info.rulebookDescription = rulebookDescription;
-			info.metaCategories = new List<AbilityMetaCategory>()
-			{
-				AbilityMetaCategory.Part1Modular, AbilityMetaCategory.Part1Rulebook
-			};
+namespace APIPlugin;
 
-			return info;
-		}
-	}
+[Obsolete("Use AbilityManager and extension methods instead", true)]
+public class AbilityInfoUtils
+{
+    public static AbilityInfo CreateInfoWithDefaultSettings(string rulebookName, string rulebookDescription)
+    {
+        AbilityInfo info = ScriptableObject.CreateInstance<AbilityInfo>();
+        info.powerLevel = 0;
+        info.rulebookName = rulebookName;
+        info.rulebookDescription = rulebookDescription;
+        info.metaCategories = new List<AbilityMetaCategory>()
+        {
+            AbilityMetaCategory.Part1Modular, AbilityMetaCategory.Part1Rulebook
+        };
+
+        return info;
+    }
 }
--- InscryptionAPI/Compatibility/TailIdentifier.cs
+++ InscryptionAPI/Compatibility/TailIdentifier.cs
@@ -1,62 +1,60 @@
 using DiskCardGame;
-using InscryptionAPI.Card;
 using InscryptionAPI.Helpers;
 using UnityEngine;
 
-namespace APIPlugin
+namespace APIPlugin;
+
+[Obsolete("Use CardManager and CardInfo extension methods", true)]
+public class TailIdentifier
 {
-    [Obsolete("Use CardManager and CardInfo extension methods", true)]
-	public class TailIdentifier
-	{
-		internal string name;
-		internal CardModificationInfo mods;
-		internal Texture2D tailLostTex;
-		private TailParams tail;
+    internal string name;
+    internal CardModificationInfo mods;
+    internal Texture2D tailLostTex;
+    private TailParams tail;
 
-		public TailParams Tail
-		{
-			get
-			{
-				if (this.tail == null)
-                    SetParams(CardLoader.GetCardByName(this.name));
+    public TailParams Tail
+    {
+        get
+        {
+            if (this.tail == null)
+                SetParams(CardLoader.GetCardByName(this.name));
 				
-				return this.tail;
-			}
-		}
+            return this.tail;
+        }
+    }
 
-		public TailIdentifier(string name, Texture2D tailLostTex = null, CardModificationInfo mods = null)
-		{
-			this.name = name;
-			this.mods = mods;
-			this.tailLostTex = tailLostTex;
-		}
+    public TailIdentifier(string name, Texture2D tailLostTex = null, CardModificationInfo mods = null)
+    {
+        this.name = name;
+        this.mods = mods;
+        this.tailLostTex = tailLostTex;
+    }
 
-		private void SetParams(CardInfo card)
-		{
-			TailParams tail = new TailParams();
+    private void SetParams(CardInfo card)
+    {
+        TailParams tail = new TailParams();
 
-			if (this.tailLostTex is not null)
-			{
-				this.tailLostTex.name = this.name;
-				this.tailLostTex.filterMode = FilterMode.Point;
+        if (this.tailLostTex is not null)
+        {
+            this.tailLostTex.name = this.name;
+            this.tailLostTex.filterMode = FilterMode.Point;
 
-				tail.tailLostPortrait = TextureHelper.ConvertTexture(this.tailLostTex, TextureHelper.SpriteType.CardPortrait);
-				tail.tailLostPortrait.name = this.name;
-			}
+            tail.tailLostPortrait = TextureHelper.ConvertTexture(this.tailLostTex, TextureHelper.SpriteType.CardPortrait);
+            tail.tailLostPortrait.name = this.name;
+        }
 
-			tail.tail = card;
+        tail.tail = card;
 
-			if (this.mods != null)
-			{
-				tail.tail.mods.Add(this.mods);
-			}
+        if (this.mods != null)
+        {
+            tail.tail.mods.Add(this.mods);
+        }
 
-			this.tail = tail;
-		}
+        this.tail = tail;
+    }
 
-		public override string ToString()
-		{
-			return name;
-		}
-	}
+    public override string ToString()
+    {
+        return name;
+    }
 }
