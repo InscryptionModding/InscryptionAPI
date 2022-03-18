469c7c0e07bda45dbc64b5c3eedd59b386a8f0b9
julian-perge <julianperge@gmail.com>
2022-03-18 02:46:15 -0400

    version 2.0.1

--- InscryptionAPI/InscryptionAPIPlugin.cs
+++ InscryptionAPI/InscryptionAPIPlugin.cs
@@ -19,7 +19,7 @@ public class InscryptionAPIPlugin : BaseUnityPlugin
 {
     public const string ModGUID = "cyantist.inscryption.api";
     public const string ModName = "InscryptionAPI";
-    public const string ModVer = "2.0.0";
+    public const string ModVer = "2.0.1";
 
     private static bool _hasShownOldApiWarning = false;
     
