0f2ccdaeb52f0f0ebfe40f0ceed4914823feaec9
julian-perge <julianperge@gmail.com>
2022-04-02 07:41:03 -0400

    Refactored and cleaned up latch patch.
    Also fixed prefix throwing exceptions as there was no annotation for the Prefix.

--- InscryptionCommunityPatch/Card/Act1LatchAbilityFix.cs
+++ InscryptionCommunityPatch/Card/Act1LatchAbilityFix.cs
@@ -1,7 +1,7 @@
+using System.Collections;
 using DiskCardGame;
 using HarmonyLib;
 using Pixelplacement;
-using System.Collections;
 using UnityEngine;
 
 namespace InscryptionCommunityPatch.Card;
@@ -16,44 +16,41 @@ public class Act1LatchAbilityFix
         {
             if (_clawPrefab == null)
                 _clawPrefab = ResourceBank.Get<GameObject>("Prefabs/Cards/SpecificCardModels/LatchClaw");
-            
+
             return _clawPrefab;
         }
     }
 
     [HarmonyPatch(typeof(BoardManager), nameof(BoardManager.ChooseTarget))]
     [HarmonyPostfix]
-    public static IEnumerator ReEnanbleSlots(IEnumerator result, BoardManager __instance, List<CardSlot> allTargets)
+    public static IEnumerator ReEnableSlots(IEnumerator result, BoardManager __instance, List<CardSlot> allTargets)
     {
         List<CardSlot> validSlots = __instance.currentValidSlots;
         __instance.currentValidSlots = null;
         (__instance.GetComponent<SelectTargetHolder>() ?? __instance.gameObject.AddComponent<SelectTargetHolder>()).isSelectingTarget = true;
         List<CardSlot> toDisable = new();
-        foreach (CardSlot cs in allTargets)
+        foreach (CardSlot cs in allTargets.Where(cs => !cs.Enabled))
         {
-            if (!cs.Enabled)
-            {
-                cs.SetEnabled(true);
-                toDisable.Add(cs);
-            }
+            cs.SetEnabled(true);
+            toDisable.Add(cs);
         }
         yield return result;
         foreach (CardSlot cs in toDisable)
         {
             cs.SetEnabled(false);
         }
-            (__instance.GetComponent<SelectTargetHolder>() ?? __instance.gameObject.AddComponent<SelectTargetHolder>()).isSelectingTarget = false;
+        (__instance.GetComponent<SelectTargetHolder>() ?? __instance.gameObject.AddComponent<SelectTargetHolder>()).isSelectingTarget = false;
         __instance.currentValidSlots = validSlots;
-        yield break;
     }
 
     [HarmonyPatch(typeof(CardSlot), nameof(CardSlot.OnCursorEnter))]
     [HarmonyPostfix]
     public static void NoSacrificeMarker(CardSlot __instance)
     {
-        if ((Singleton<BoardManager>.Instance?.GetComponent<SelectTargetHolder>()?.isSelectingTarget).GetValueOrDefault())
+        if (__instance.Card && BoardManager.Instance
+            && (BoardManager.Instance.GetComponent<SelectTargetHolder>()?.isSelectingTarget).GetValueOrDefault())
         {
-            __instance.Card?.Anim?.SetSacrificeHoverMarkerShown(false);
+            __instance.Card.Anim.SetSacrificeHoverMarkerShown(false);
         }
     }
 
@@ -62,28 +59,26 @@ public class Act1LatchAbilityFix
         public bool isSelectingTarget;
     }
 
-    private static void AimWeaponAnim(GameObject TweenObj, Vector3 target) => Tween.LookAt(TweenObj.transform, target, Vector3.up, 0.075f, 0.0f, Tween.EaseInOut);
+    private static void AimWeaponAnim(GameObject tweenObj, Vector3 target) => Tween.LookAt(tweenObj.transform, target, Vector3.up, 0.075f, 0.0f, Tween.EaseInOut);
 
-    [HarmonyPrefix]
-    public static void Prefix1(out Latch __state, ref Latch __instance) => __state = __instance;
+    [HarmonyPrefix, HarmonyPatch(typeof(Latch), nameof(Latch.OnPreDeathAnimation))]
+    public static void PrefixPassStateOnPreDeath(out Latch __state, ref Latch __instance) => __state = __instance;
 
-    [HarmonyPatch(typeof(Latch), nameof(Latch.OnPreDeathAnimation))]
-    [HarmonyPostfix]
+    [HarmonyPostfix, HarmonyPatch(typeof(Latch), nameof(Latch.OnPreDeathAnimation))]
     public static IEnumerator Postfix(IEnumerator enumerator, Latch __state, bool wasSacrifice)
     {
-        if (SceneLoader.ActiveSceneName != "Part1_Cabin")
+        if (SceneLoader.ActiveSceneName != "Part1_Cabin" || !SceneLoader.ActiveSceneName.StartsWith("finale"))
         {
             yield return enumerator;
             yield break;
         }
 
-        PatchPlugin.Logger.LogInfo("Started death");
-        PatchPlugin.Logger.LogInfo(__state.name);
+        PatchPlugin.Logger.LogInfo($"[LatchFix] Started death, latch name: [{__state.name}]");
 
         List<CardSlot> validTargets = BoardManager.Instance.AllSlotsCopy;
         validTargets.RemoveAll(slot => slot.Card == null || slot.Card.Dead || __state.CardHasLatchMod(slot.Card) || slot.Card == __state.Card);
 
-        PatchPlugin.Logger.LogInfo("Count of Valid Targets : " + validTargets.Count.ToString());
+        PatchPlugin.Logger.LogInfo("[LatchFix] Count of Valid Targets : " + validTargets.Count);
 
         if (validTargets.Count > 0)
         {
@@ -94,28 +89,30 @@ public class Act1LatchAbilityFix
 
             CardAnimationController anim = __state.Card.Anim;
 
-            GameObject gameObject = new GameObject();
-            gameObject.name = "LatchParent";
-            gameObject.transform.position = anim.transform.position;
-            gameObject.gameObject.transform.parent = anim.transform;
+            GameObject latchParentGameObject = new GameObject
+            {
+                name = "LatchParent",
+                transform =
+                {
+                    position = anim.transform.position
+                }
+            };
+            latchParentGameObject.transform.SetParent(anim.transform);
 
-            Transform Latchparent = gameObject.transform;
-            GameObject claw = UnityEngine.Object.Instantiate(ClawPrefab, Latchparent);
-            Material cannonmat = null;
+            Transform latchParent = latchParentGameObject.transform;
+            GameObject claw = UnityObject.Instantiate(ClawPrefab, latchParent);
+            Material cannonMat = null;
             try
             {
-                cannonmat = new Material(ResourceBank.Get<GameObject>("Prefabs/Cards/SpecificCardModels/CannonTargetIcon").GetComponentInChildren<Renderer>().material);
+                cannonMat = new Material(ResourceBank.Get<GameObject>("Prefabs/Cards/SpecificCardModels/CannonTargetIcon").GetComponentInChildren<Renderer>().material);
             }
             catch { }
-            if (cannonmat != null)
+            if (cannonMat != null)
             {
                 Renderer[] renderers = claw.GetComponentsInChildren<Renderer>();
-                foreach (Renderer rend in renderers)
+                foreach (Renderer rend in renderers.Where(rend => rend))
                 {
-                    if (rend != null)
-                    {
-                        rend.material = cannonmat;
-                    }
+                    rend.material = cannonMat;
                 }
             }
 
@@ -128,7 +125,7 @@ public class Act1LatchAbilityFix
 
                 if (selectedSlot != null && selectedSlot.Card != null)
                 {
-                    AimWeaponAnim(Latchparent.gameObject, selectedSlot.transform.position);
+                    AimWeaponAnim(latchParent.gameObject, selectedSlot.transform.position);
                     yield return new WaitForSeconds(0.3f);
                 }
             }
@@ -137,30 +134,39 @@ public class Act1LatchAbilityFix
                 List<CardSlot> allSlotsCopy = BoardManager.Instance.AllSlotsCopy;
                 allSlotsCopy.Remove(__state.Card.Slot);
 
-                yield return BoardManager.Instance.ChooseTarget(allSlotsCopy, validTargets, 
+                yield return BoardManager.Instance.ChooseTarget(allSlotsCopy, validTargets,
                     s => selectedSlot = s, // target selected callback
                     __state.OnInvalidTarget, // invalid target callback
-                    s =>                    // slot cusor enter callback
+                    s => // slot cursor enter callback
                     {
                         if (s.Card == null)
                             return;
 
-                        AimWeaponAnim(Latchparent.gameObject, s.transform.position);
-                    }, 
-                    null,                   // cancel condition
+                        AimWeaponAnim(latchParent.gameObject, s.transform.position);
+                    },
+                    null, // cancel condition
                     CursorType.Target);
             }
 
             claw.SetActive(true);
 
-            CustomCoroutine.FlickerSequence(() => claw.SetActive(true), () => claw.SetActive(false), true, false, 0.05f, 2);
+            CustomCoroutine.FlickerSequence(
+                () => claw.SetActive(true),
+                () => claw.SetActive(false),
+                true, 
+                false, 
+                0.05f, 
+                2
+            );
 
             if (selectedSlot != null && selectedSlot.Card != null)
             {
-                CardModificationInfo mod = new CardModificationInfo(__state.LatchAbility);
-                mod.fromCardMerge = true;
-                mod.fromLatch = true;
-                PatchPlugin.Logger.LogInfo(selectedSlot.Card.name);
+                CardModificationInfo mod = new CardModificationInfo(__state.LatchAbility)
+                {
+                    fromCardMerge = true,
+                    fromLatch = true
+                };
+                PatchPlugin.Logger.LogInfo($"[LatchFix] Selected card name [{selectedSlot.Card.name}]");
 
                 if (selectedSlot.Card.Info.name == "!DEATHCARD_BASE")
                 {
