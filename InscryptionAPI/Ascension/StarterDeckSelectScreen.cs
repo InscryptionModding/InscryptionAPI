df1fb51d75c8f8d13abc177934353db3fb32c467
SpecialAPI <pavlov.andrei.d@gmail.com>
2022-04-23 12:49:15 +0300

    deck at the start

--- InscryptionAPI/Ascension/StarterDeckSelectScreen.cs
+++ InscryptionAPI/Ascension/StarterDeckSelectScreen.cs
@@ -1,5 +1,8 @@
 using DiskCardGame;
+using GBC;
 using HarmonyLib;
+using InscryptionAPI.Helpers;
+using System.Reflection;
 using UnityEngine;
 
 namespace InscryptionAPI.Ascension;
@@ -9,7 +12,7 @@ internal static class StarterDeckSelectscreenPatches
 {
     [HarmonyPatch(typeof(AscensionChooseStarterDeckScreen), "Start")]
     [HarmonyPostfix]
-    public static void Postfix(AscensionChooseStarterDeckScreen __instance)
+    private static void Postfix(AscensionChooseStarterDeckScreen __instance)
     {
         if (__instance.GetComponent<StarterDeckPaginator>() == null)
         {
@@ -89,5 +92,82 @@ internal static class StarterDeckSelectscreenPatches
                 rightArrow.GetComponent<AscensionMenuInteractable>().CursorSelectStarted += (x) => manager.NextPage();
             }
         }
+    }
+
+    private static Sprite noneCard;
+
+    [HarmonyPatch(typeof(AscensionChooseStarterDeckScreen), nameof(AscensionChooseStarterDeckScreen.OnCursorEnterDeckIcon))]
+    [HarmonyPrefix]
+    private static bool FixDeckPreview(AscensionChooseStarterDeckScreen __instance, AscensionStarterDeckIcon icon)
+    {
+        var screenPlus = __instance.GetComponent<StarterDeckScreenPlus>() ?? __instance.gameObject.AddComponent<StarterDeckScreenPlus>();
+        screenPlus.tempCards?.ForEach(x => x.SetActive(false));
+        if(icon.starterDeckInfo == null)
+        {
+            screenPlus.noneCardObjects ??= new();
+            if (screenPlus.noneCardObjects.Count <= 0)
+            {
+                foreach (var card in __instance.cardLockedSprites)
+                {
+                    var cloned = UnityObject.Instantiate(card.gameObject);
+                    cloned.transform.parent = card.transform.parent;
+                    cloned.transform.localPosition = card.transform.localPosition;
+                    cloned.GetComponent<SpriteRenderer>().sprite = noneCard ??= TextureHelper.GetImageAsTexture("ascension_card_none_darker.png", Assembly.GetExecutingAssembly()).ConvertTexture();
+                    cloned.GetComponent<SpriteRenderer>().enabled = true;
+                    screenPlus.noneCardObjects.Add(cloned);
+                }
+            }
+            screenPlus.noneCardObjects.ForEach(x => x.SetActive(true));
+            __instance.cardLockedSprites.ForEach(x => x.gameObject.SetActive(false));
+        }
+        else
+        {
+            screenPlus?.noneCardObjects.ForEach(x => x.SetActive(false));
+            __instance.cardLockedSprites.ForEach(x => x.gameObject.SetActive(true));
+            if(icon.Unlocked && icon.starterDeckInfo.cards != null && icon.starterDeckInfo.cards.Count != __instance.cards.Count)
+            {
+                __instance.cardLockedSprites.ForEach(delegate (SpriteRenderer x)
+                {
+                    x.enabled = false;
+                });
+                __instance.cards.ForEach(delegate (PixelSelectableCard x)
+                {
+                    x.gameObject.SetActive(false);
+                });
+                var firstOrDefault = __instance.cards.FirstOrDefault();
+                if(firstOrDefault != null)
+                {
+                    screenPlus.tempCards ??= new();
+                    var cards = icon.starterDeckInfo.cards;
+                    for (int i = 0; i < cards.Count; i++)
+                    {
+                        var card = cards[i];
+                        float distance = 0.5f;
+                        float position = -((cards.Count - 1) / 2 * distance) + distance * i;
+                        if(Math.Abs(position) < 2.6f)
+                        {
+                            var cloned = screenPlus.tempCards.Find(x => !x.activeSelf)?.GetComponentInChildren<PixelSelectableCard>() ?? UnityObject.Instantiate(firstOrDefault);
+                            cloned.transform.parent = firstOrDefault.transform.parent;
+                            cloned.transform.localPosition = new(position, firstOrDefault.transform.localPosition.y, firstOrDefault.transform.localPosition.z);
+                            cloned.SetInfo(card);
+                            cloned.SetEnabled(false);
+                            cloned.gameObject.SetActive(true);
+                            if (!screenPlus.tempCards.Contains(cloned.gameObject))
+                            {
+                                screenPlus.tempCards.Add(cloned.gameObject);
+                            }
+                        }
+                    }
+                    __instance.StopAllCoroutines();
+                    __instance.StartCoroutine(__instance.ShowCardsSequence());
+                    return false;
+                }
+            }
+        }
+        __instance.cards.ForEach(delegate (PixelSelectableCard x)
+        {
+            x.gameObject.SetActive(true);
+        });
+        return true;
     }
 }
