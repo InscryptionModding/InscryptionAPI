0124b6a2e03a0b4a86b9faff0e15e7b4b23e849e
SpecialAPI <pavlov.andrei.d@gmail.com>
2022-04-24 21:50:26 +0300

    changed setup

--- InscryptionAPI/Ascension/AscensionChallengePaginator.cs
+++ InscryptionAPI/Ascension/AscensionChallengePaginator.cs
@@ -1,5 +1,6 @@
 using DiskCardGame;
 using InscryptionAPI.Helpers;
+using InscryptionAPI.Helpers.Extensions;
 using System.Reflection;
 using UnityEngine;
 
@@ -8,6 +9,7 @@ namespace InscryptionAPI.Ascension;
 public class AscensionChallengePaginator : MonoBehaviour
 {
     public static Sprite missingChallengeSprite;
+    public bool initialized;
 
     public void Initialize(AscensionChallengeScreen screen, AscensionMenuScreenTransition transition = null)
     {
@@ -39,6 +41,8 @@ public class AscensionChallengePaginator : MonoBehaviour
         {
             this.transition = transition;
         }
+        initialized = true;
+        OnEnable();
     }
 
     public void AddPage(List<AscensionChallengeInfo> page)
@@ -131,7 +135,90 @@ public class AscensionChallengePaginator : MonoBehaviour
 
     public void OnEnable()
     {
+        if (!initialized)
+        {
+            return;
+        }
         ChallengeManager.SyncChallengeList();
+        if(rightArrow)
+            Destroy(rightArrow);
+        if(leftArrow)
+            Destroy(leftArrow);
+        foreach(KeyValuePair<int, List<GameObject>> kvp in challengeObjectsForPages)
+        {
+            if(kvp.Key != 0)
+            {
+                kvp.Value.ForEach(x => Destroy(x));
+            }
+        }
+        List<ChallengeManager.FullChallenge> fcs = ChallengeManager.NewInfos.ToList();
+        fcs.Sort((x, x2) => x.SortValue != x2.SortValue ? x.SortValue - x2.SortValue : x2.UnlockLevel - x.UnlockLevel);
+        List<AscensionChallengeInfo> challengesToAdd = new(fcs.ConvertAll(x => x.Challenge.Repeat(x.AppearancesInChallengeScreen)).SelectMany(x => x));
+        List<AscensionIconInteractable> icons = challengeObjectsForPages[0].ConvertAll(x => x.GetComponent<AscensionIconInteractable>()).FindAll(x => x != null);
+        icons.ForEach(delegate (AscensionIconInteractable ic)
+        {
+            if (ic != null && ic.Info == null && challengesToAdd.Count > 0)
+            {
+                ic.challengeInfo = challengesToAdd[0];
+                ic.AssignInfo(challengesToAdd[0]);
+                challengesToAdd.RemoveAt(0);
+            }
+        });
+        List<List<AscensionChallengeInfo>> pagesToAdd = new();
+        while (challengesToAdd.Count > 0)
+        {
+            List<AscensionChallengeInfo> page = new();
+            for (int i = 0; i < icons.Count; i++)
+            {
+                if (challengesToAdd.Count > 0)
+                {
+                    page.Add(challengesToAdd.Last());
+                    challengesToAdd.RemoveAt(challengesToAdd.Count - 1);
+                }
+            }
+            pagesToAdd.Add(page);
+        }
+        if (pagesToAdd.Count > 0)
+        {
+            foreach (List<AscensionChallengeInfo> page in pagesToAdd)
+            {
+                AddPage(page);
+            }
+            Vector3 topRight = new(float.MinValue, float.MinValue);
+            Vector3 bottomLeft = new(float.MaxValue, float.MaxValue);
+            foreach (AscensionIconInteractable icon in icons)
+            {
+                if (icon != null && icon.iconRenderer != null)
+                {
+                    if (icon.iconRenderer.transform.position.x < bottomLeft.x)
+                    {
+                        bottomLeft.x = icon.iconRenderer.transform.position.x;
+                    }
+                    if (icon.iconRenderer.transform.position.x > topRight.x)
+                    {
+                        topRight.x = icon.iconRenderer.transform.position.x;
+                    }
+                    if (icon.iconRenderer.transform.position.y < bottomLeft.y)
+                    {
+                        bottomLeft.y = icon.iconRenderer.transform.position.y;
+                    }
+                    if (icon.iconRenderer.transform.position.y > topRight.y)
+                    {
+                        topRight.y = icon.iconRenderer.transform.position.y;
+                    }
+                }
+            }
+            leftArrow = UnityEngine.Object.Instantiate((screen?.gameObject ?? transform.gameObject).GetComponentInParent<AscensionMenuScreens>().cardUnlockSummaryScreen.GetComponent<AscensionCardsSummaryScreen>().pageLeftButton.gameObject);
+            leftArrow.transform.parent = screen?.transform ?? transform.transform;
+            leftArrow.transform.position = Vector3.Lerp(new Vector3(bottomLeft.x, topRight.y, topRight.z), new Vector3(bottomLeft.x, bottomLeft.y, topRight.z), 0.5f) + Vector3.left / 3f;
+            leftArrow.GetComponent<AscensionMenuInteractable>().ClearDelegates();
+            leftArrow.GetComponent<AscensionMenuInteractable>().CursorSelectStarted += (x) => PreviousPage();
+            rightArrow = UnityEngine.Object.Instantiate((screen?.gameObject ?? transform.gameObject).GetComponentInParent<AscensionMenuScreens>().cardUnlockSummaryScreen.GetComponent<AscensionCardsSummaryScreen>().pageRightButton.gameObject);
+            rightArrow.transform.parent = screen?.transform ?? transform.transform;
+            rightArrow.transform.position = Vector3.Lerp(new Vector3(topRight.x, topRight.y, topRight.z), new Vector3(topRight.x, bottomLeft.y, topRight.z), 0.5f) + Vector3.right / 3f;
+            rightArrow.GetComponent<AscensionMenuInteractable>().ClearDelegates();
+            rightArrow.GetComponent<AscensionMenuInteractable>().CursorSelectStarted += (x) => NextPage();
+        }
     }
 
     public int pageIndex;
@@ -140,6 +227,8 @@ public class AscensionChallengePaginator : MonoBehaviour
     public AscensionChallengeScreen screen;
     private AscensionChallengeInfo missing;
     public AscensionMenuScreenTransition transition;
+    public GameObject rightArrow;
+    public GameObject leftArrow;
 
     private class NoneChallengeDisplayer : MonoBehaviour
     {
--- InscryptionAPI/Ascension/AscensionChallengePaginatorSetupifier.cs
+++ InscryptionAPI/Ascension/AscensionChallengePaginatorSetupifier.cs
@@ -19,76 +19,9 @@ namespace InscryptionAPI.Ascension
                     x.GetComponent<AscensionIconInteractable>()));
                 if (icons.Count > 0 && challengeScreen.GetComponent<AscensionChallengePaginator>() == null)
                 {
+                    ChallengeManager.SyncChallengeList();
                     AscensionChallengePaginator manager = challengeScreen.gameObject.AddComponent<AscensionChallengePaginator>();
                     manager.Initialize(null, screen);
-                    ChallengeManager.SyncChallengeList();
-                    List<ChallengeManager.FullChallenge> fcs = ChallengeManager.NewInfos.ToList();
-                    fcs.Sort((x, x2) => x.SortValue != x2.SortValue ? x.SortValue - x2.SortValue : x2.UnlockLevel - x.UnlockLevel);
-                    List<AscensionChallengeInfo> challengesToAdd = new(fcs.ConvertAll(x => x.Challenge.Repeat(x.AppearancesInChallengeScreen)).SelectMany(x => x));
-                    icons.ForEach(delegate (AscensionIconInteractable ic)
-                    {
-                        if (ic != null && ic.Info == null && challengesToAdd.Count > 0)
-                        {
-                            ic.challengeInfo = challengesToAdd[0];
-                            ic.AssignInfo(challengesToAdd[0]);
-                            challengesToAdd.RemoveAt(0);
-                        }
-                    });
-                    List<List<AscensionChallengeInfo>> pagesToAdd = new();
-                    while (challengesToAdd.Count > 0)
-                    {
-                        List<AscensionChallengeInfo> page = new();
-                        for (int i = 0; i < icons.Count; i++)
-                        {
-                            if (challengesToAdd.Count > 0)
-                            {
-                                page.Add(challengesToAdd.Last());
-                                challengesToAdd.RemoveAt(challengesToAdd.Count - 1);
-                            }
-                        }
-                        pagesToAdd.Add(page);
-                    }
-                    if (pagesToAdd.Count > 0)
-                    {
-                        foreach (List<AscensionChallengeInfo> page in pagesToAdd)
-                        {
-                            manager.AddPage(page);
-                        }
-                        Vector3 topRight = new(float.MinValue, float.MinValue);
-                        Vector3 bottomLeft = new(float.MaxValue, float.MaxValue);
-                        foreach (AscensionIconInteractable icon in icons)
-                        {
-                            if (icon != null && icon.iconRenderer != null)
-                            {
-                                if (icon.iconRenderer.transform.position.x < bottomLeft.x)
-                                {
-                                    bottomLeft.x = icon.iconRenderer.transform.position.x;
-                                }
-                                if (icon.iconRenderer.transform.position.x > topRight.x)
-                                {
-                                    topRight.x = icon.iconRenderer.transform.position.x;
-                                }
-                                if (icon.iconRenderer.transform.position.y < bottomLeft.y)
-                                {
-                                    bottomLeft.y = icon.iconRenderer.transform.position.y;
-                                }
-                                if (icon.iconRenderer.transform.position.y > topRight.y)
-                                {
-                                    topRight.y = icon.iconRenderer.transform.position.y;
-                                }
-                            }
-                        }
-                        GameObject leftArrow = Instantiate(challengeScreen.GetComponentInParent<AscensionMenuScreens>().cardUnlockSummaryScreen.GetComponent<AscensionCardsSummaryScreen>().pageLeftButton.gameObject);
-                        leftArrow.transform.parent = challengeScreen.transform;
-                        leftArrow.transform.position = Vector3.Lerp(new Vector3(bottomLeft.x, topRight.y, topRight.z), new Vector3(bottomLeft.x, bottomLeft.y, topRight.z), 0.5f) + (Vector3.left / 3f);
-                        leftArrow.GetComponent<AscensionMenuInteractable>().ClearDelegates();
-                        leftArrow.GetComponent<AscensionMenuInteractable>().CursorSelectStarted += (x) => manager.PreviousPage();
-                        GameObject rightArrow = Instantiate(challengeScreen.GetComponentInParent<AscensionMenuScreens>().cardUnlockSummaryScreen.GetComponent<AscensionCardsSummaryScreen>().pageRightButton.gameObject);
-                        rightArrow.transform.parent = challengeScreen.transform;
-                        rightArrow.transform.position = Vector3.Lerp(new Vector3(topRight.x, topRight.y, topRight.z), new Vector3(topRight.x, bottomLeft.y, topRight.z), 0.5f) + (Vector3.right / 3f);
-                        rightArrow.GetComponent<AscensionMenuInteractable>().ClearDelegates();
-                        rightArrow.GetComponent<AscensionMenuInteractable>().CursorSelectStarted += (x) => manager.NextPage();
-                    }
                 }
             }
         }
