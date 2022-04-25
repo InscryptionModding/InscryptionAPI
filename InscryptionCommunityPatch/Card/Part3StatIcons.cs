ddbb6748d2ae3a7f605741e042724bcdd9a53120
divisionbyz0rro <divisionbyzorro@gmail.com>
2022-04-25 13:32:32 -0500

    moved magic numbers to constants

--- InscryptionCommunityPatch/Card/Part3StatIcons.cs
+++ InscryptionCommunityPatch/Card/Part3StatIcons.cs
@@ -7,6 +7,17 @@ namespace InscryptionCommunityPatch.Card;
 [HarmonyPatch]
 internal static class Part3StatIcons
 {
+    // This activates the renderers for the special stat icons on the disk cards (where appropriate)
+    // scales the icons appropriately
+    // set the material color
+    // sets the game objects to the correct layer (where appropriate)
+    // and in some cases swaps the x position of the health and attack icons because they were wrong for some reason
+
+    private static readonly Vector3 ICON_SCALE = new Vector3(0.2f, 0.2f, 1f);
+    private static readonly Vector3 LEFT_ICON = new Vector3(-0.39f, 0.033f, 0f);
+    private static readonly Vector3 RIGHT_ICON = new Vector3(0.39f, 0.033f, 0f);
+    private static readonly Color ICON_COLOR = GameColors.Instance.brightBlue;
+
     [HarmonyPatch(typeof(DiskCardGame.Card), "RenderCard")]
     [HarmonyPrefix]
     private static void UpdateLiveRenderedCard(ref DiskCardGame.Card __instance)
@@ -14,14 +25,14 @@ internal static class Part3StatIcons
         if (SaveManager.SaveFile.IsPart3)
         {
             CardStatIcons icons = __instance.gameObject.GetComponentInChildren<CardStatIcons>();
-            icons.attackIconRenderer.gameObject.transform.localPosition = new(0.39f, 0.033f, 0f);
-            icons.attackIconRenderer.gameObject.transform.localScale = new (0.2f, 0.2f, 1f);
-            icons.attackIconRenderer.material.color = GameColors.Instance.brightBlue;
+            icons.attackIconRenderer.gameObject.transform.localPosition = RIGHT_ICON; // Flipped for some reason??
+            icons.attackIconRenderer.gameObject.transform.localScale = ICON_SCALE;
+            icons.attackIconRenderer.material.color = ICON_COLOR;
             icons.attackIconRenderer.enabled = false;
 
-            icons.healthIconRenderer.gameObject.transform.localPosition = new(-0.39f, 0.033f, 0f);
-            icons.healthIconRenderer.gameObject.transform.localScale = new (0.2f, 0.2f, 1f);
-            icons.healthIconRenderer.material.color = GameColors.Instance.brightBlue;
+            icons.healthIconRenderer.gameObject.transform.localPosition = LEFT_ICON;  // Flipped for some reason??
+            icons.healthIconRenderer.gameObject.transform.localScale = ICON_SCALE;
+            icons.healthIconRenderer.material.color = ICON_COLOR;
             icons.healthIconRenderer.enabled = false;
         }
     }
@@ -33,16 +44,16 @@ internal static class Part3StatIcons
         if (__instance is DiskScreenCardDisplayer)
         {
             CardStatIcons icons = __instance.gameObject.GetComponentInChildren<CardStatIcons>();
-            icons.attackIconRenderer.gameObject.transform.localPosition = new(-0.39f, 0.033f, 0f);
-            icons.attackIconRenderer.gameObject.transform.localScale = new (0.2f, 0.2f, 1f);
+            icons.attackIconRenderer.gameObject.transform.localPosition = LEFT_ICON;
+            icons.attackIconRenderer.gameObject.transform.localScale = ICON_SCALE;
             icons.attackIconRenderer.gameObject.layer = LayerMask.NameToLayer("CardOffscreenEmission");
-            icons.attackIconRenderer.material.color = GameColors.Instance.brightBlue;
+            icons.attackIconRenderer.material.color = ICON_COLOR;
             icons.attackIconRenderer.enabled = true;
 
-            icons.healthIconRenderer.gameObject.transform.localPosition = new(0.39f, 0.033f, 0f);
-            icons.healthIconRenderer.gameObject.transform.localScale = new (0.2f, 0.2f, 1f);
+            icons.healthIconRenderer.gameObject.transform.localPosition = RIGHT_ICON;
+            icons.healthIconRenderer.gameObject.transform.localScale = ICON_SCALE;
             icons.healthIconRenderer.gameObject.layer = LayerMask.NameToLayer("CardOffscreenEmission");
-            icons.healthIconRenderer.material.color = GameColors.Instance.brightBlue;
+            icons.healthIconRenderer.material.color = ICON_COLOR;
             icons.healthIconRenderer.enabled = true;
         }
     }
@@ -54,16 +65,16 @@ internal static class Part3StatIcons
         if (SaveManager.SaveFile.IsPart3)
         {
             CardStatIcons icons = __instance.gameObject.GetComponentInChildren<CardStatIcons>();
-            icons.attackIconRenderer.gameObject.transform.localPosition = new(0.39f, 0.033f, 0f);
-            icons.attackIconRenderer.gameObject.transform.localScale = new (0.2f, 0.2f, 1f);
+            icons.attackIconRenderer.gameObject.transform.localPosition = RIGHT_ICON; // Flipped for some reason??
+            icons.attackIconRenderer.gameObject.transform.localScale = ICON_SCALE;
             icons.attackIconRenderer.gameObject.layer = LayerMask.NameToLayer("CardOffscreenEmission");
-            icons.attackIconRenderer.material.color = GameColors.Instance.brightBlue;
+            icons.attackIconRenderer.material.color = ICON_COLOR;
             icons.attackIconRenderer.enabled = true;
 
-            icons.healthIconRenderer.gameObject.transform.localPosition = new(-0.39f, 0.033f, 0f);
-            icons.healthIconRenderer.gameObject.transform.localScale = new (0.2f, 0.2f, 1f);
+            icons.healthIconRenderer.gameObject.transform.localPosition = LEFT_ICON; // Flipped for some reason??
+            icons.healthIconRenderer.gameObject.transform.localScale = ICON_SCALE;
             icons.healthIconRenderer.gameObject.layer = LayerMask.NameToLayer("CardOffscreenEmission");
-            icons.healthIconRenderer.material.color = GameColors.Instance.brightBlue;
+            icons.healthIconRenderer.material.color = ICON_COLOR;
             icons.healthIconRenderer.enabled = true;
         }
     }
