71eda669f1ac33428c7e0176d4116f20e26e109e
jamesveug <james.veugelaers@outlook.co.nz>
2022-10-26 22:41:09 +1300

    Multiple default types work now

--- InscryptionAPI/Items/ConsumableItemManager.cs
+++ InscryptionAPI/Items/ConsumableItemManager.cs
@@ -31,52 +31,57 @@ public static class ConsumableItemManager
 #region Patches
 
     [HarmonyPatch(typeof(ResourceBank), "Awake", new System.Type[] { })]
-    internal class ResourceBank_Awake
+    private class ResourceBank_Awake
     {
         public static void Postfix(ResourceBank __instance)
         {
-            // The resource bank has been cleared. refill it
-            if (defaultItemModel == null)
-            {
-                Initialize();
-            }
+            InscryptionAPIPlugin.Logger.LogInfo("[ResourceBank_Awake] Postfix");
+            Initialize();
         }
     }
     
     [HarmonyPatch(typeof(ItemsUtil), "AllConsumables", MethodType.Getter)]
-    internal class ItemsUtil_AllConsumables
+    private class ItemsUtil_AllConsumables
     {
         public static void Postfix(ref List<ConsumableItemData> __result)
         {
-            __result.AddRange(NewConsumableItems);
+            __result.AddRange(allNewItems);
         }
     }
     
 #endregion
     
-    private static Dictionary<ModelType, GameObject> typeToPrefabLookup = new();
     private static GameObject defaultItemModel = null;
-    private static List<ConsumableItemData> items = new();
-    public static ReadOnlyCollection<ConsumableItemData> NewConsumableItems = new(items);
+    private static ModelType defaultItemModelType = ModelType.Hover;
+    private static Dictionary<ModelType, GameObject> typeToPrefabLookup = new();
+    private static HashSet<ModelType> defaultModelTypes = new();
+    private static List<ConsumableItemData> allNewItems = new();
+    private static List<ConsumableItemData> baseConsumableItemsDatas = new();
+    public static ReadOnlyCollection<ConsumableItemData> NewConsumableItemDatas = new(allNewItems);
 
     private static void InitializeDefaultModels()
     {
-        LoadModel("runeroundedbottom", "RuneRoundedBottom", ModelType.Basic);
-        LoadModel("customitem", "RuneRoundedBottomVeins", ModelType.BasicVeins);
-        LoadModel("customhoveringitem", "RuneHoveringItem", ModelType.Hover);
+        LoadDefaultModel("runeroundedbottom", "RuneRoundedBottom", ModelType.Basic);
+        LoadDefaultModel("customitem", "RuneRoundedBottomVeins", ModelType.BasicVeins);
+        LoadDefaultModel("customhoveringitem", "RuneHoveringItem", ModelType.Hover);
+        InscryptionAPIPlugin.Logger.LogInfo("[InitializeDefaultModels] Default items " + typeToPrefabLookup.Count);
     }
 
-    private static void LoadModel(string assetBundlePath, string prefabName, ModelType type)
+    private static void LoadDefaultModel(string assetBundlePath, string prefabName, ModelType type)
     {
         byte[] resourceBytes = TextureHelper.GetResourceBytes(assetBundlePath, typeof(InscryptionAPIPlugin).Assembly);
         if (AssetBundleHelper.TryGet(resourceBytes, prefabName, out GameObject prefab))
         {
             typeToPrefabLookup[type] = prefab;
+            defaultModelTypes.Add(type);
         }
     }
 
     private static void Initialize()
     {
+        InscryptionAPIPlugin.Logger.LogInfo("[InitializeDefaultModels] Initializing");
+        baseConsumableItemsDatas.Clear();
+        baseConsumableItemsDatas.AddRange(ItemsUtil.AllConsumables);
         if (InscryptionAPIPlugin.configCustomItemTypes.Value == ConsumableState.Vanilla)
         {
             // Don't change any items!
@@ -85,43 +90,25 @@ public static class ConsumableItemManager
         
         if (defaultItemModel == null)
         {
-            // TODO: Get the actual custom item model
             // NOTE: Do not add a ConsumableItem component here so we can override default models with this!
             InitializeDefaultModels();
-            defaultItemModel = typeToPrefabLookup[ModelType.Basic];
+            defaultItemModel = typeToPrefabLookup[defaultItemModelType];
         }
-
-        List<ConsumableItemData> baseConsumableItems = ItemsUtil.AllConsumables;
-
+        
         // Add all totem tops to the game
-        foreach (ConsumableItemData item in items)
+        foreach (ConsumableItemData item in allNewItems)
         {
-            ModelType modelType = item.GetPrefabModelType();
-            if (!typeToPrefabLookup.TryGetValue(modelType, out GameObject prefab))
-            {
-                // No model assigned. use default model!
-                prefab = defaultItemModel;
-            }
-            
-            // TODO: Do not error if this is a custom model!
-            GameObject gameObject = CloneBasePrefab(item, prefab);
-
-            SetupPrefab(gameObject, item.GetComponentType());
-
-            ResourceBank.instance.resources.Add(new ResourceBank.Resource()
-            {
-                path = "Prefabs/Items/" + item.prefabId,
-                asset = gameObject
-            });
+            InitializeConsumableItemDataPrefab(item);
         }
         
         // Override vanilla items
         if (InscryptionAPIPlugin.configCustomItemTypes.Value == ConsumableState.All)
         {
             // Change all base items to use the fallback model!
-            foreach (ConsumableItemData data in baseConsumableItems)
+            foreach (ConsumableItemData data in baseConsumableItemsDatas)
             {
-                if (NewConsumableItems.Contains(data) || !CanUseBaseModel(data))
+                InscryptionAPIPlugin.Logger.LogInfo("[InitializeDefaultModels] Overriding data " + data.name);
+                if (!CanUseBaseModel(data))
                 {
                     continue;
                 }
@@ -134,8 +121,7 @@ public static class ConsumableItemManager
                     continue;
                 }
 
-                GameObject cloneBasePrefab = CloneBasePrefab(data, defaultItemModel);
-                ConsumableItem cloneConsumableItem = SetupPrefab(cloneBasePrefab, prefabConsumableItem.GetType());
+                ConsumableItem clone = CloneAndSetupPrefab(data, defaultItemModel, prefabConsumableItem.GetType(), defaultItemModelType);
 
                 bool replaced = false;
                 for (int i = 0; i < ResourceBank.instance.resources.Count; i++)
@@ -143,23 +129,39 @@ public static class ConsumableItemManager
                     ResourceBank.Resource resource = ResourceBank.instance.resources[i];
                     if (resource.path.ToLowerInvariant() == path)
                     {
-                        resource.asset = cloneBasePrefab;
+                        resource.asset = clone.gameObject;
                         replaced = true;
                         break;
                     }
                 }
                 if (!replaced)
                 {
-                    MoveComponent(prefabConsumableItem, cloneConsumableItem);
+                    MoveComponent(prefabConsumableItem, clone);
                     
                     ResourceBank.instance.resources.Add(new ResourceBank.Resource()
                     {
                         path = path,
-                        asset = cloneBasePrefab
+                        asset = clone.gameObject
                     });
                 }
             }
         }
+    }
+    private static void InitializeConsumableItemDataPrefab(ConsumableItemData item)
+    {
+        ModelType modelType = item.GetPrefabModelType();
+        if (!typeToPrefabLookup.TryGetValue(modelType, out GameObject prefab))
+        {
+            // No model assigned. use default model!
+            prefab = defaultItemModel;
+        }
+
+        GameObject gameObject = CloneAndSetupPrefab(item, prefab, item.GetComponentType(), modelType).gameObject;
+        ResourceBank.instance.resources.Add(new ResourceBank.Resource()
+        {
+            path = "Prefabs/Items/" + item.prefabId,
+            asset = gameObject
+        });
     }
 
     private static bool CanUseBaseModel(ConsumableItemData data)
@@ -180,47 +182,45 @@ public static class ConsumableItemManager
         }
     }
 
-    private static GameObject CloneBasePrefab(ConsumableItemData data, GameObject gameObject)
+    private static ConsumableItem CloneAndSetupPrefab(ConsumableItemData data, GameObject prefab, Type itemType, ModelType modelType)
     {
-        GameObject prefab = GameObject.Instantiate(gameObject);
-        prefab.name = $"Custom Item ({data.rulebookName})";
-                
-        // Populate icon
-        if (data.rulebookSprite != null)
+        GameObject clone = GameObject.Instantiate(prefab);
+        clone.name = $"Custom Item ({data.rulebookName})";
+        
+        // Populate icon. Only for default fallback types - If anyone wants to use this then they can add to that fallback type list i guss??
+        if (defaultModelTypes.Contains(modelType))
         {
-            GameObject icon = prefab.gameObject.FindChild("Icon");
-            if (icon != null)
+            if (data.rulebookSprite != null)
             {
-                Renderer iconRenderer = icon.GetComponent<Renderer>();
-                if (iconRenderer != null)
+                GameObject icon = clone.gameObject.FindChild("Icon");
+                if (icon != null)
                 {
-                    iconRenderer.material.mainTexture = data.rulebookSprite.texture;
+                    Renderer iconRenderer = icon.GetComponent<Renderer>();
+                    if (iconRenderer != null)
+                    {
+                        iconRenderer.material.mainTexture = data.rulebookSprite.texture;
+                    }
+                    else
+                    {
+                        InscryptionAPIPlugin.Logger.LogError($"Could not find Renderer on Icon GameObject to assign tribe icon!");
+                    }
                 }
                 else
                 {
-                    InscryptionAPIPlugin.Logger.LogError($"Could not find Renderer on Icon GameObject to assign tribe icon!");
+                    InscryptionAPIPlugin.Logger.LogError($"Could not find Icon GameObject to assign tribe icon!");
                 }
             }
             else
             {
-                InscryptionAPIPlugin.Logger.LogError($"Could not find Icon GameObject to assign tribe icon!");
+                InscryptionAPIPlugin.Logger.LogError($"Could not change icon for {data.rulebookName}. No sprite defined!");
             }
         }
-        else
-        {
-            InscryptionAPIPlugin.Logger.LogError($"Could not change icon for {data.rulebookName}. No sprite defined!");
-        }
 
-        return prefab;
-    }
-
-    private static ConsumableItem SetupPrefab(GameObject prefab, Type itemType)
-    {
-        // Add default animation
-        Animator animator = prefab.GetComponentInChildren<Animator>();
+        // Add default animation if it doesn't have one
+        Animator animator = clone.GetComponentInChildren<Animator>();
         if (animator == null)
         {
-            Transform child = prefab.transform.GetChild(0);
+            Transform child = clone.transform.GetChild(0);
             if (child != null)
             {
                 animator = child.gameObject.AddComponent<Animator>();
@@ -236,14 +236,19 @@ public static class ConsumableItemManager
             animator.Rebind();
         }
 
-        ConsumableItem consumableItem = prefab.GetComponent<ConsumableItem>();
+        // Add component if it doesn't exist
+        ConsumableItem consumableItem = clone.GetComponent<ConsumableItem>();
         if (consumableItem == null)
         {
-            consumableItem = (ConsumableItem)prefab.AddComponent(itemType);
+            consumableItem = clone.AddComponent(itemType) as ConsumableItem;
+            if (consumableItem == null)
+            {
+                InscryptionAPIPlugin.Logger.LogError($"Type given is not a ConsumableItem! You may encounter unexpected bugs");
+            }
         }
 
         // Mark as dont destroy on load so it doesn't get removed between levels
-        UnityObject.DontDestroyOnLoad(prefab);
+        UnityObject.DontDestroyOnLoad(clone);
 
         return consumableItem;
     }
@@ -299,7 +304,8 @@ public static class ConsumableItemManager
         data.SetComponentType(itemType);
         data.SetPowerLevel(1);
         
-        items.Add(data);
+        InscryptionAPIPlugin.Logger.LogInfo("[InitializeDefaultModels] Recorded new item " + name);
+        allNewItems.Add(data);
         return data;
     }
 }
