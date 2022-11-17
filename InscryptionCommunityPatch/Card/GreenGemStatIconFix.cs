bf917cce31800ca6d6926bc90a0babd1cf3f4cf2
jamesveug <james.veugelaers@outlook.co.nz>
2022-11-17 23:07:10 +1300

    Fixed Green Gem stat icon appearing as a black square

---
+++ InscryptionCommunityPatch/Card/GreenGemStatIconFix.cs
@@ -0,0 +1,24 @@
+﻿using DiskCardGame;
+using HarmonyLib;
+using UnityEngine;
+using InscryptionAPI.Helpers;
+
+namespace InscryptionCommunityPatch.Card;
+
+[HarmonyPatch]
+public static class GreenGemStatIconFix
+{
+    /// <summary>
+    /// This fixes the Green Gem stat icon from appearing as a black square but instead as a gem
+    /// </summary>
+    [HarmonyPatch(typeof(StatIconInfo), nameof(StatIconInfo.LoadAbilityData))]
+    [HarmonyPostfix]
+    private static void StatIconInfo_LoadAbilityData()
+    {
+        StatIconInfo greenGemStatIcon = StatIconInfo.allIconInfo.Find((a) => a.name == "GreenGemsStatIcon");
+        
+        Texture2D baseTexture = TextureHelper.GetImageAsTexture("GreenGem.png", typeof(GreenGemStatIconFix).Assembly);
+        greenGemStatIcon.iconGraphic = baseTexture;
+        greenGemStatIcon.rulebookDescription = greenGemStatIcon.gbcDescription;
+    }
+}
