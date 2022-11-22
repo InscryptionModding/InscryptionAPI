3edb0288affa0aa66c5bb37b66c4cb012cc23c4c
WhistleWind <humanhaterq@gmail.com>
2022-11-21 18:42:10 -0800

    Rewrote to use a transpiler

--- InscryptionCommunityPatch/Sequencers/PackRatNodeBackgroundFix.cs
+++ InscryptionCommunityPatch/Sequencers/PackRatNodeBackgroundFix.cs
@@ -1,59 +1,99 @@
 using DiskCardGame;
 using HarmonyLib;
-using Pixelplacement;
-using System.Collections;
+using InscryptionCommunityPatch.Card;
+using System.Reflection;
+using System.Reflection.Emit;
 using UnityEngine;
 
 namespace InscryptionCommunityPatch.Sequencers;
 
+[HarmonyPatch]
 internal class PackRatNodeBackgroundFix
 {
-    [HarmonyPatch(typeof(GainConsumablesSequencer), nameof(GainConsumablesSequencer.FullConsumablesSequence))]
-    private class GainConsumablesPatch
+    private const string name_RatCard = "DiskCardGame.SelectableCard ratCard";
+    private const string name_Reward = "DiskCardGame.CardInfo fullConsumablesReward";
+    private const string name_PixelPlacement = "Pixelplacement.TweenSystem.TweenBase Position(UnityEngine.Transform, UnityEngine.Vector3, Single, Single, UnityEngine.AnimationCurve, LoopType, System.Action, System.Action, Boolean)";
+    private const string name_GetComponent = "DiskCardGame.SelectableCard GetComponent[SelectableCard]()";
+    private static MethodBase TargetMethod()
     {
-        private static IEnumerator Postfix(IEnumerator enumerator, GainConsumablesSequencer __instance)
+        MethodBase baseMethod = AccessTools.Method(typeof(GainConsumablesSequencer), nameof(GainConsumablesSequencer.FullConsumablesSequence));
+        return AccessTools.EnumeratorMoveNext(baseMethod);
+    }
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        List<CodeInstruction> codes = new List<CodeInstruction>(instructions);
+
+        object op_RatCard = null;
+        object op_Reward = null;
+
+        // we want to slowly narrow our search until we find exactly where we want to insert our code
+        for (int i = 0; i < codes.Count; i++)
         {
-            yield return new WaitForSeconds(1f);
-            yield return Singleton<TextDisplayer>.Instance.PlayDialogueEvent("GainConsumablesFull", TextDisplayer.MessageAdvanceMode.Input, TextDisplayer.EventIntersectMode.Wait, new string[1] { RunState.Run.MaxConsumables.ToString() });
-            yield return new WaitForSeconds(0.5f);
-            __instance.rat.SetActive(value: true);
-            yield return new WaitUntil(() => __instance.ratCard.gameObject.activeInHierarchy);
-            bool cardGrabbed = false;
-            __instance.ratCard.SetInfo(__instance.fullConsumablesReward);
-            __instance.ratCard.ClearDelegates();
-            SelectableCard selectableCard = __instance.ratCard;
-            selectableCard.CursorSelectEnded = (Action<MainInputInteractable>)Delegate.Combine(selectableCard.CursorSelectEnded, (Action<MainInputInteractable>)delegate
+            // get the ratCard operand
+            if (codes[i].opcode == OpCodes.Ldfld && codes[i].operand.ToString() == name_RatCard)
+                op_RatCard = codes[i].operand;
+
+            if (codes[i].opcode == OpCodes.Ldfld && codes[i].operand.ToString() == name_Reward)
+                op_Reward = codes[i].operand;
+
+            // look for the original code for `component`
+            if (codes[i].opcode == OpCodes.Callvirt && codes[i].operand.ToString() == name_GetComponent)
             {
-                cardGrabbed = true;
-            });
-            yield return new WaitUntil(() => cardGrabbed);
-            Singleton<RuleBookController>.Instance.SetShown(shown: false);
-            Singleton<TableRuleBook>.Instance.SetOnBoard(onBoard: false);
-            __instance.rat.GetComponentInChildren<Animator>().SetTrigger("exit");
-            yield return new WaitForEndOfFrame();
-            GameObject cardObj = UnityEngine.Object.Instantiate(__instance.ratCard.gameObject);
-            cardObj.SetActive(value: true);
-            cardObj.transform.parent = null;
-            cardObj.transform.position = __instance.ratCard.transform.position;
-            cardObj.transform.rotation = __instance.ratCard.transform.rotation;
-            cardObj.transform.localScale = Vector3.one;
-            SelectableCard component = SelectableCard.Instantiate(__instance.ratCard);
-            component.SetInteractionEnabled(interactionEnabled: false);
-            string text = __instance.fullConsumablesReward.description;
-            if (ProgressionData.LearnedCard(__instance.fullConsumablesReward))
-            {
-                text = string.Format(Localization.Translate("A [c:bR]{0}[c:]... Always useful."), __instance.fullConsumablesReward.DisplayedNameLocalized);
+                int startIndex = -1, endIndex = -1;
+                for (int j = i; j > 0; j--)
+                {
+                    // find the startIndex
+                    if (codes[j].opcode == OpCodes.Ldarg_0)
+                    {
+                        startIndex = j;
+                        break;
+                    }
+                }
+
+                // find the endIndex
+                for (int k = i; k < codes.Count; k++)
+                {
+                    if (codes[k].opcode == OpCodes.Stloc_2)
+                    {
+                        for (int l = k; l > 0; l--)
+                        {
+                            if (codes[l].opcode == OpCodes.Callvirt)
+                            {
+                                endIndex = l + 1;
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                }
+
+                MethodInfo customMethod = AccessTools.Method(typeof(PackRatNodeBackgroundFix), nameof(PackRatNodeBackgroundFix.InstantiateSelectableCard), new Type[] { typeof(SelectableCard), typeof(CardInfo) });
+
+                // remove all the old code
+                codes.RemoveRange(startIndex, endIndex - startIndex);
+
+                // gainConsumablesSequence.ratCard
+                codes.Insert(startIndex, new CodeInstruction(OpCodes.Ldloc_1));
+                codes.Insert(startIndex + 1, new CodeInstruction(OpCodes.Ldfld, op_RatCard));
+
+                // gainConsumablesSequence.fullConsumablesReward
+                codes.Insert(startIndex + 2, new CodeInstruction(OpCodes.Ldloc_1));
+                codes.Insert(startIndex + 3, new CodeInstruction(OpCodes.Ldfld, op_Reward));
+
+                // InstantiateSelectableCard
+                codes.Insert(startIndex + 4, new CodeInstruction(OpCodes.Call, customMethod));
+                break;
             }
-            yield return __instance.LearnObjectSequence(cardObj.transform, 1f, new Vector3(20f, 0f, 0f), text);
-            Tween.Position(cardObj.transform, cardObj.transform.position + Vector3.up * 2f + Vector3.forward * 0.5f, 0.1f, 0f, null, Tween.LoopType.None, null, delegate
-            {
-                UnityEngine.Object.Destroy(cardObj);
-                UnityEngine.Object.Destroy(component);
-            });
-            yield return new WaitForSeconds(0.5f);
-            __instance.rat.SetActive(value: false);
-            RunState.Run.playerDeck.AddCard(__instance.fullConsumablesReward);
-            yield break;
         }
+
+        return codes;
+    }
+
+    public static void InstantiateSelectableCard(SelectableCard card, CardInfo info)
+    {
+        SelectableCard component = SelectableCard.Instantiate(card);
+        component.SetInfo(info);
+        component.SetInteractionEnabled(false);
+        SelectableCard.Destroy(component);
     }
 }
