0605b169cb5ac0574a19ccb936f837031d5cf80c
jamesveug <james.veugelaers@outlook.co.nz>
2022-12-25 15:04:33 +1300

    Added ResourceBankManager so there's less fluffing about adding new models

---
+++ InscryptionAPI/ResourceBank/ResourceBankManager.cs
@@ -0,0 +1,65 @@
+﻿using HarmonyLib;
+using Sirenix.Utilities;
+
+namespace DiskCardGame;
+
+public static class ResourceBankManager
+{
+    private class ResourceData
+    {
+        public ResourceBank.Resource Resource;
+        public bool OverrideExistingResource;
+    }
+    
+    private static List<ResourceData> customResources = new List<ResourceData>();
+
+    public static void AddResource(ResourceBank.Resource resource, bool overrideExistingAsset=false)
+    {
+        if (resource == null)
+        {
+            InscryptionAPI.InscryptionAPIPlugin.Logger.LogError("Cannot add null resources!");
+        }
+        else if (string.IsNullOrEmpty(resource.path))
+        {
+            InscryptionAPI.InscryptionAPIPlugin.Logger.LogWarning($"Attempting to add resource with empty path! '{resource.path}' and asset {resource.asset}");
+        }
+        else
+        {
+            customResources.Add(new ResourceData()
+            {
+                Resource = resource,
+                OverrideExistingResource = overrideExistingAsset
+            });
+        }
+    }
+    
+    [HarmonyPatch(typeof (ResourceBank), "Awake", new System.Type[] {})]
+    public class ResourceBank_Awake
+    {
+        public static void Postfix(ResourceBank __instance)
+        {
+            Dictionary<string, ResourceBank.Resource> existingPaths = __instance.resources.ToDictionary((a)=>a.path, (a)=>a);
+            foreach (ResourceData resourceData in customResources)
+            {
+                string resourcePath = resourceData.Resource.path;
+                if (existingPaths.TryGetValue(resourcePath, out ResourceBank.Resource resource) && !resourceData.OverrideExistingResource)
+                {
+                    if (resourceData.OverrideExistingResource)
+                    {
+                        resource.asset = resourceData.Resource.asset;
+                        continue;
+                    }
+                    else
+                    {
+                        InscryptionAPI.InscryptionAPIPlugin.Logger.LogWarning("Resource added with that already exists! " + resourcePath);
+                    }
+                }
+                else
+                {
+                    existingPaths[resourcePath] = resourceData.Resource;
+                }
+                __instance.resources.Add(resourceData.Resource);
+            }
+        }
+    }
+}
