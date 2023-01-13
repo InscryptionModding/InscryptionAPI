894ee139939c4c1a492582c1712735f08f853da6
KBMackenzie <109933089+KBMackenzie@users.noreply.github.com>
2023-01-13 18:39:49 -0300

    Added documentation and made small changes

--- InscryptionAPI/TalkingCards/Create/FaceData.cs
+++ InscryptionAPI/TalkingCards/Create/FaceData.cs
@@ -9,11 +9,25 @@ using InscryptionAPI.TalkingCards.Helpers;
 #nullable enable
 namespace InscryptionAPI.TalkingCards.Create;
 
+/// <summary>
+/// Data for the creation of a talking card through this API.
+/// </summary>
 public class FaceData
 {
+    /// <summary>
+    /// The name of an existing card.
+    /// </summary>
     public string CardName { get; }
+
+    /// <summary>
+    /// Your talking card's emotions.
+    /// </summary>
     public List<EmotionData> Emotions { get; }
     public EmotionData Neutral => Emotions.Find(x => x.Emotion == Emotion.Neutral);
+
+    /// <summary>
+    /// A bit of info about your talking card: blink rate and voice.
+    /// </summary>
     public FaceInfo? FaceInfo { get; set; }
 
     public FaceData(string cardName, List<EmotionData> emotions, FaceInfo? faceInfo)
@@ -24,6 +38,9 @@ public class FaceData
     }
 }
 
+/// <summary>
+/// A bit of information about a talking card: blink rate and voice.
+/// </summary>
 [Serializable]
 public class FaceInfo
 {
@@ -32,9 +49,24 @@ public class FaceInfo
     /* Yes, I know these names should be capitalized.
      * However, camelCase looks better in JSON files, so I'm gonna make this compromise. :'3 */
 
+    /// <summary>
+    /// How often your character should blink.
+    /// </summary>
     public float? blinkRate { get; set; }
+
+    /// <summary>
+    /// Your character's voice ID.
+    /// </summary>
     public string? voiceId { get; set; }
+
+    /// <summary>
+    /// Your character's voice's pitch.
+    /// </summary>
     public float? voiceSoundPitch { get; set; }
+
+    /// <summary>
+    /// The path to a short audio file to be used as your character's voice.
+    /// </summary>
     public string? customVoice { get; set; }
 
     public FaceInfo(float? blinkRate = null, string? voiceId = null, float? voiceSoundPitch = null, string? customVoice = null)
@@ -45,10 +77,10 @@ public class FaceInfo
         this.customVoice = customVoice;
     }
 
-    public float GetBlinkRate()
+    internal float GetBlinkRate()
         => Mathf.Clamp(blinkRate ?? GeneratePortrait.BlinkRate, 0.1f, 10f);
 
-    public float GetVoicePitch()
+    internal float GetVoicePitch()
         => Mathf.Clamp(voiceSoundPitch ?? GeneratePortrait.VoicePitch, 0.1f, 10f);
 
     private string? CustomVoice()
@@ -57,7 +89,7 @@ public class FaceInfo
         return result ? customVoice : null;
     }
 
-    public string GetVoiceId()
+    internal string GetVoiceId()
         => CustomVoice()
         ?? (
             (voiceId != null && ValidVoiceIds.Contains(voiceId))
@@ -66,12 +98,34 @@ public class FaceInfo
         );
 }
 
+/// <summary>
+/// A talking card's emotion (a facial expression, basically). A container for sprites.
+/// </summary>
 public class EmotionData
 {
+    /// <summary>
+    /// The chosen emotion.
+    /// </summary>
     public Emotion Emotion { get; }
+
+    /// <summary>
+    /// A sprite for your talking card's face.
+    /// </summary>
     public Sprite Face { get; }
+
+    /// <summary>
+    /// A pair of sprites for your talking card's eyes: open and closed, respectively.
+    /// </summary>
     public FaceAnim Eyes { get; }
+
+    /// <summary>
+    /// A pair of sprites for your talking card's mouth: open and closed, respectively.
+    /// </summary>
     public FaceAnim Mouth { get; }
+
+    /// <summary>
+    /// A pair of sprites for your talking card's emission: open and closed, respectively.
+    /// </summary>
     public FaceAnim Emission { get; }
 
     public EmotionData(Emotion emotion, Sprite face, FaceAnim eyes, FaceAnim mouth, FaceAnim emission)
@@ -127,7 +181,7 @@ public class EmotionData
     }
     #endregion
 
-    public CharacterFace.EmotionSprites MakeEmotion() => new()
+    internal CharacterFace.EmotionSprites MakeEmotion() => new()
     {
         emotion = Emotion,
         face = Face,
@@ -142,12 +196,19 @@ public class EmotionData
         eyesClosedEmission = Emission.Closed
     };
 
-    public static Sprite EmptyPortrait => GeneratePortrait.EmptyPortrait;
-
     public static implicit operator EmotionData((Emotion emotion, Sprite sprite) x)
-        => new(x.emotion, x.sprite, EmptyPortrait, EmptyPortrait, EmptyPortrait);
+        => new(
+                x.emotion,
+                x.sprite,
+                GeneratePortrait.EmptyPortraitTuple,
+                GeneratePortrait.EmptyPortraitTuple,
+                GeneratePortrait.EmptyPortraitTuple
+            );
 }
 
+/// <summary>
+/// A container for sprites for a part of a talking card's face.
+/// </summary>
 public class FaceAnim
 {
     public Sprite Open { get; }
@@ -167,9 +228,6 @@ public class FaceAnim
 
     public static implicit operator Sprite(FaceAnim x)
         => x.Closed;
-
-    public static implicit operator FaceAnim(Sprite s)
-        => new FaceAnim(s, s);
 
     public static implicit operator FaceAnim((Sprite? open, Sprite? closed) x)
         => new FaceAnim(x.open, x.closed);
