4f5216c7f898b97da9e5c28a445b81e7543f2df6
WhistleWind <humanhaterq@gmail.com>
2023-01-14 11:10:21 -0800

    2.9.1

--- InscryptionAPI/InscryptionAPIPlugin.cs
+++ InscryptionAPI/InscryptionAPIPlugin.cs
@@ -28,7 +28,7 @@ public class InscryptionAPIPlugin : BaseUnityPlugin
 {
     public const string ModGUID = "cyantist.inscryption.api";
     public const string ModName = "InscryptionAPI";
-    public const string ModVer = "2.9.0";
+    public const string ModVer = "2.9.1";
 
     internal static ConfigEntry<TotemManager.TotemTopState> configCustomTotemTopTypes;
     internal static ConfigEntry<ConsumableItemManager.ConsumableState> configCustomItemTypes;
