e0827f3f138cd9ddb9fb30b2b3c021affa30dd75
James <james.veugelaers@outlook.co.nz>
2023-10-13 08:07:08 +1300

    More helpers for regions

--- InscryptionAPI/Regions/Part1RegionData.cs
+++ InscryptionAPI/Regions/Part1RegionData.cs
@@ -1,12 +1,12 @@
+using System.Reflection;
 using DiskCardGame;
+using InscryptionAPI.Guid;
 
 namespace InscryptionAPI.Regions;
 
 public class Part1RegionData
 {
-    private int tier;
-    private RegionData region;
-
+    public string GUID { get; private set; }
     public int Tier { get => tier; private set => tier = value; }
     public RegionData Region { get => region; private set => region = value; }
     public int MinTerrain { get; set; }
@@ -18,8 +18,14 @@ public class Part1RegionData
     public bool AllowLockedTerrainCards { get; set; }
     public bool AllowSacrificableTerrainCards { get; set; }
 
+    private int tier;
+    private RegionData region;
+    
     public Part1RegionData(RegionData region, int tier)
     {
+        Assembly callingAssembly = Assembly.GetCallingAssembly();
+        GUID = TypeManager.GetModIdFromCallstack(callingAssembly);
+        
         AllowSacrificableTerrainCards = false;
         AllowLockedTerrainCards = false;
         MinTerrain = -2;
